658503e12a44df70e238b2175cdfd8dd0334060e
yamamaura_tatusya <t.yamamura@gemcook.com>
2019-08-09 02:31:24 +0900

    [fin] Lecture20-22 親のAppが子のSearchFormコンポーネントから状態を受け取る

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,33 +1,23 @@
 import React, { Component } from 'react';
 
-import Greeting from './greeting';
+import SearchForm from './SearchForm';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Mr.Tatsuyaaaa',
     };
   }
 
-  handleNameChange(e) {
-    this.setState({ name: e.target.value });
-  }
-
-  handleBobClicked() {
-    this.setState({ name: 'Bob' });
+  handlePlaceSubmit(place) {
+    console.log(place);
   }
 
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={e => this.handleNameChange(e)}
-        />
-        <button onClick={() => this.handleBobClicked()}>I am Tatsuya</button>
-        <Greeting name={this.state.name} />
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
       </div>
     );
   }
