47ade819c1bb951e703dc1029d99acbf31bc5bf2
yamamaura_tatusya <t.yamamura@gemcook.com>
2019-08-09 01:46:17 +0900

    [fin] Lecture18 state完全（さっきのはprops）

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,11 +3,28 @@ import React, { Component } from 'react';
 import Greeting from './greeting';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Mr.Tatsuyaaaa',
+    };
+  }
+
+  handleMouseOver() {
+    this.setState({ name: 'Tatsuya' });
+  }
+
+  handleMouseOut() {
+    this.setState({ name: 'Yamamura' });
+  }
+
   render() {
     return (
-      <div>
-        <Greeting name="Tatsuya" />
-        <Greeting name="Yamamura" />
+      <div
+        onMouseOver={() => this.handleMouseOver()}
+        onMouseOut={() => this.handleMouseOut()}
+      >
+        <Greeting name={this.state.name} />
       </div>
     );
   }
